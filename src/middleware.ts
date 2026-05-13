96cd8b6e14aeb1fac6a2f52f9a5e4d70ae05329f
Priyanka D <priyanka.d@mindsprint.org>
2026-05-13 15:42:56 +0530

    header issue fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,9 @@ import type { NextRequest } from 'next/server';
 import i18nConfig from '@src/i18n/config';
 
 export function middleware(request: NextRequest) {
-  const response = i18nRouter(request, i18nConfig);
-
-  if (!response.headers.has('X-Content-Type-Options')) {
-    response.headers.set('X-Content-Type-Options', 'nosniff');
-  }
-
-  if (!response.headers.has('X-Frame-Options')) {
-    response.headers.set('X-Frame-Options', 'SAMEORIGIN');
-  }
-
-  if (!response.headers.has('Referrer-Policy')) {
-    response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
-  }
-
-  return response;
+  return i18nRouter(request, i18nConfig);
 }
 
 export const config = {
-  matcher: ['/((?!api|contentful-image|static|.*\\..*|_next).*)', '/_next/image'],
+  matcher: '/((?!api|contentful-image|static|.*\\..*|_next).*)',
 };
